ed2888cade5ef31ab46d3f773263d7473da33ab0
yellingviv <viv@yellingviv.com>
2021-12-02 15:57:16 -0800

    starting day 2

--- day2.py
+++ day2.py
@@ -1,18 +1,21 @@
-# considering three-part sums increasing
+# calculate the movement forward, up, down, etc then multiply the final position
 
-input = open('input-day1.txt', 'r')
-data = []
+input = open('input-day2.txt', 'r')
+directions = []
 for line in input:
-    data.append(int(line.strip()))
+    directions.append(line.split())
 
-increases = 0
+horizontal = 0
+depth = 0
 
-for i in range(0, len(data)-3):
-    triad = sum(data[i:i+3])
-    next = sum(data[i+1:i+4])
-    if next > triad:
-        increases += 1
+for i in range(0, len(directions)):
+    if directions[i][0] == 'forward':
+        horizontal = horizontal + int(directions[i][1])
+    elif directions[i][0] == 'down':
+        depth = depth + int(directions[i][1])
+    elif directions[i][0] == 'up':
+        depth = depth - int(directions[i][1])
 
-print(f'Final result: {increases}')
-
-# for line in data:
+print(f'Final horizontal distance: {horizontal}')
+print(f'Final depth: {depth}')
+print(f'And if you multiply them... {horizontal * depth}')
