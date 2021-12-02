c7e0fb1a2713c3bed09fd811e4a84f7d5ac744a4
yellingviv <viv@yellingviv.com>
2021-12-01 18:00:19 -0800

    i resent the way they calculate this but i got it right

--- day1.py
+++ day1.py
@@ -5,16 +5,20 @@ data = open('input-day1.txt', 'r')
 counter = 0
 prev = 0
 increases = 0
+decreases = 0
 
 for line in data:
     if counter > 0:
         if line > prev:
             increases += 1
             prev = line
-        else:
+        elif line < prev:
             prev = line
+            decreases += 1
     elif counter == 0:
         prev = line
     counter += 1
 
+print(f'Total counters: {counter}')
+print(f'Final decrease count: {decreases}')
 print(f'Final increase count: {increases}')
